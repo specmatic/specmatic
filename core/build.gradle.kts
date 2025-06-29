8c1d6720dff9074508e7eff4355482c58cc01f20
Specmatic Renovate <github-service-account@specmatic.io>
2025-06-29 02:25:57 +0000

    chore(deps): update dependency org.junit.jupiter:junit-jupiter-params to v5.13.2

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation("org.assertj:assertj-core:3.27.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.2")
     testImplementation("org.json:json:20250517")
     testImplementation("org.springframework:spring-web:6.1.14")
     testImplementation("io.mockk:mockk:1.13.11")
