04774af6c843cbf45311530e5c677979866a8ee1
Specmatic Renovate <github-service-account@specmatic.io>
2025-06-29 02:25:50 +0000

    chore(deps): update dependency org.junit.jupiter:junit-jupiter-api to v5.13.2

--- junit5-support/build.gradle.kts
+++ junit5-support/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("org.junit.jupiter:junit-jupiter-engine:5.11.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("org.assertj:assertj-core:3.27.3")
-    implementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.13.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.0")
     implementation("org.thymeleaf:thymeleaf:3.1.3.RELEASE")
 
