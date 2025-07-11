ad59166bd75e4c06a4755caaf5c02c922786d400
Sufiyan <StarKhan6368@gmail.com>
2025-07-11 18:52:16 +0530

    Make Scalar interfaces inherit main interfaces
    - ScalarValue inherits Value
    - ScalarType inherits Pattern
    - QueryParameterScalarPattern inherits ScalarType

--- core/src/main/kotlin/io/specmatic/core/pattern/QueryParameterScalarPattern.kt
+++ core/src/main/kotlin/io/specmatic/core/pattern/QueryParameterScalarPattern.kt
@@ -6,7 +6,7 @@ import io.specmatic.core.Substitution
 import io.specmatic.core.utilities.exceptionCauseMessage
 import io.specmatic.core.value.*
 
-data class QueryParameterScalarPattern(override val pattern: Pattern): Pattern by pattern {
+data class QueryParameterScalarPattern(override val pattern: Pattern): Pattern by pattern, ScalarType {
     override fun resolveSubstitutions(
         substitution: Substitution,
         value: Value,
