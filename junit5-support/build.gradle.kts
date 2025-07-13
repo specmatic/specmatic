6c5dcb2ddb2a0ffbe76abfe1fdb27afa749ab998
Specmatic Renovate <github-service-account@specmatic.io>
2025-07-13 02:37:45 +0000

    chore(deps): update ktor monorepo to v3

--- junit5-support/build.gradle.kts
+++ junit5-support/build.gradle.kts
@@ -20,8 +20,8 @@ dependencies {
 
     implementation("org.thymeleaf:thymeleaf:3.1.3.RELEASE")
 
-    implementation("io.ktor:ktor-client-core-jvm:2.3.13")
-    implementation("io.ktor:ktor-client-cio:2.3.13")
+    implementation("io.ktor:ktor-client-core-jvm:3.2.1")
+    implementation("io.ktor:ktor-client-cio:3.2.1")
 
     implementation("org.junit.platform:junit-platform-launcher:1.13.3")
     implementation("org.junit.platform:junit-platform-reporting:1.13.3")
