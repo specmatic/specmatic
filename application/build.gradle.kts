4523c81868582cc570408f1acec73800d9fa4e19
Specmatic Renovate <github-service-account@specmatic.io>
2025-07-20 02:31:08 +0000

    chore(deps): update dependency io.mockk:mockk to v1.14.5

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.3")
 
-    testImplementation("io.mockk:mockk:1.13.11")
+    testImplementation("io.mockk:mockk:1.14.5")
     testImplementation("org.assertj:assertj-core:3.27.3")
     testImplementation("com.ginsberg:junit5-system-exit:2.0.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.3")
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.3")
     testImplementation("org.json:json:20250517")
     testImplementation("org.springframework:spring-web:6.1.21")
-    testImplementation("io.mockk:mockk:1.13.11")
+    testImplementation("io.mockk:mockk:1.14.5")
     testImplementation("org.assertj:assertj-core:3.27.3")
     testImplementation("io.ktor:ktor-client-mock-jvm:2.3.13")
     implementation("org.thymeleaf:thymeleaf:3.1.3.RELEASE")
