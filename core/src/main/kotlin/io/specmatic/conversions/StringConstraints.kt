5acfd59cb3595307ad26af6a1445dc42cd367cc2
Joel Rosario <cirquitz@gmail.com>
2025-09-09 19:36:58 +0530

    Updated the warning printed when downsampling maxLength or minLength

--- core/src/main/kotlin/io/specmatic/conversions/StringConstraints.kt
+++ core/src/main/kotlin/io/specmatic/conversions/StringConstraints.kt
@@ -5,8 +5,12 @@ import io.swagger.v3.oas.models.media.StringSchema
 
 private const val REASONABLE_STRING_LENGTH = 4 * 1024 * 1024
 
-class StringConstraints(schema: StringSchema, patternName: String, breadCrumb: String) {
-    val resolvedBreadCrumb = if (patternName.isNotBlank()) "schema $patternName" else breadCrumb
+class StringConstraints(
+    schema: StringSchema,
+    patternName: String,
+    breadCrumb: String,
+) {
+    private val resolvedBreadCrumb = if (patternName.isNotBlank()) "schema $patternName" else breadCrumb
     val resolvedMaxLength: Int?
     val downsampledMax: Boolean
     val resolvedMinLength: Int?
@@ -33,7 +37,7 @@ class StringConstraints(schema: StringSchema, patternName: String, breadCrumb: S
 
         return if (length > REASONABLE_STRING_LENGTH) {
             val warningMessage =
-                "WARNING: The $paramName of $length for $breadCrumb is very large. It may have downstream impact, such as tests do not covering the max length value of this field. Please reconsider the design of this field."
+                "WARNING: The $paramName of $length for $breadCrumb is very large. We will use a more reasonable $paramName of 4MB. Boundary testing will not be done for this parameter, and string lengths generated for test or stub will not exceed 4MB in length. Please review the $paramName of $length on this field."
             logger.log(warningMessage)
             REASONABLE_STRING_LENGTH to true
         } else {
