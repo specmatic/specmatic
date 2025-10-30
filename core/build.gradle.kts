5a0b137adc5b9bb7bf8132d2afd3f4d8f7c9295c
Specmatic Renovate <github-service-account@specmatic.io>
2025-10-31 00:31:52 +0530

    chore(deps): update dependency io.swagger.parser.v3:swagger-parser to v2.1.35

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
 
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.1")
 
-    implementation("io.swagger.parser.v3:swagger-parser:2.1.31")
+    implementation("io.swagger.parser.v3:swagger-parser:2.1.35")
 
     implementation("dk.brics:automaton:1.12-4")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
