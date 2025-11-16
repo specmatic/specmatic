f7ff2a3435f3480b227430a87ea1126fe71ed6db
Specmatic Renovate <github-service-account@specmatic.io>
2025-11-16 00:44:59 +0000

    chore(deps): update junit-framework monorepo

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     testImplementation("com.arakelian:java-jq:2.0.0")
 
     implementation("org.assertj:assertj-core:3.27.6")
-    implementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.14.1")
 
     implementation("info.picocli:picocli:4.7.7")
     implementation("io.ktor:ktor-client-core-jvm:2.3.13")
@@ -25,8 +25,8 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.19.1")
 
-    implementation("org.junit.platform:junit-platform-launcher:1.13.4")
-    implementation("org.junit.platform:junit-platform-reporting:1.13.4")
+    implementation("org.junit.platform:junit-platform-launcher:1.14.1")
+    implementation("org.junit.platform:junit-platform-reporting:1.14.1")
 
     implementation("org.eclipse.jgit:org.eclipse.jgit:7.4.0.202509020913-r")
     implementation("org.eclipse.jgit:org.eclipse.jgit.ssh.apache:7.4.0.202509020913-r")
@@ -46,13 +46,13 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.4")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.1")
 
     testImplementation("io.mockk:mockk:1.13.11")
     testImplementation("org.assertj:assertj-core:3.27.6")
     testImplementation("com.ginsberg:junit5-system-exit:2.0.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.14.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.14.1")
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.1")
 
 }
