c45748ae34bbfc5c5e0f4e2ed79a1a2a54318031
Specmatic Renovate <github-service-account@specmatic.io>
2025-11-23 00:52:26 +0000

    chore(deps): update junit-framework monorepo

--- junit5-support/build.gradle.kts
+++ junit5-support/build.gradle.kts
@@ -16,17 +16,17 @@ dependencies {
 
     implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
 
-    implementation("org.junit.jupiter:junit-jupiter-engine:5.13.4")
-    implementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.4")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.14.1")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.14.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.14.1")
 
     implementation("org.thymeleaf:thymeleaf:3.1.3.RELEASE")
 
     implementation("io.ktor:ktor-client-core-jvm:2.3.13")
     implementation("io.ktor:ktor-client-cio:2.3.13")
 
-    implementation("org.junit.platform:junit-platform-launcher:1.13.4")
-    implementation("org.junit.platform:junit-platform-reporting:1.13.4")
+    implementation("org.junit.platform:junit-platform-launcher:1.14.1")
+    implementation("org.junit.platform:junit-platform-reporting:1.14.1")
 
     implementation("org.fusesource.jansi:jansi:2.4.2")
 }
