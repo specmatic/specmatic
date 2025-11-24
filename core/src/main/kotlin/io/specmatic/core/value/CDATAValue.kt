a41eb4881dc1c346bd673d1e5566ae8f9064cdf1
Joel Rosario <cirquitz@gmail.com>
2025-11-24 19:06:03 +0530

    Update CData alter

--- core/src/main/kotlin/io/specmatic/core/value/CDATAValue.kt
+++ core/src/main/kotlin/io/specmatic/core/value/CDATAValue.kt
@@ -31,7 +31,5 @@ data class CDATAValue(
     override val nativeValue: String
         get() = stringValue.string
 
-    override fun alterValue(): ScalarValue {
-        return this
-    }
+    override fun alterValue(): ScalarValue = stringValue.alterValue()
 }
