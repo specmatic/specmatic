7f41a1952cba42b718a0324120be285302adc803
Sufiyan <StarKhan6368@gmail.com>
2025-11-24 14:00:05 +0530

    Add more tests to OpenAPI 31 vs 30 specifications

--- core/src/main/kotlin/io/specmatic/conversions/SchemaUtils.kt
+++ core/src/main/kotlin/io/specmatic/conversions/SchemaUtils.kt
@@ -1,5 +1,6 @@
 package io.specmatic.conversions
 
+import io.swagger.v3.core.util.Json31
 import io.swagger.v3.oas.models.media.JsonSchema
 import io.swagger.v3.oas.models.media.Schema
 import io.swagger.v3.parser.reference.OpenAPI31Traverser
@@ -7,8 +8,15 @@ import io.swagger.v3.parser.reference.OpenAPI31Traverser
 object SchemaUtils {
     private val openAPI31Traverser = OpenAPI31Traverser(null)
 
+    fun refHasSiblings(schema: Schema<*>): Boolean {
+        val props = Json31.mapper().convertValue(schema, MutableMap::class.java)
+        props.remove("\$ref")
+        return props.isNotEmpty()
+    }
+
     fun mergeResolvedIfJsonSchema(resolvedSchema: Schema<*>, refSchema: Schema<*>): Schema<*> {
         if (resolvedSchema !is JsonSchema) return resolvedSchema
+        if (!refHasSiblings(refSchema)) return resolvedSchema
         return mergeSchemas(resolvedSchema, refSchema)
     }
 
