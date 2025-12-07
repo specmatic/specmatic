207816b088bfb120dee550e084903250bc69f5f3
Sufiyan <StarKhan6368@gmail.com>
2025-12-07 10:43:53 +0530

    Implement FuzzyUnexpectedKeyCheck and example validator
    - Can validate rawJSON values against the external example schema
    - FuzzyUnexpectedKeyCheck can suggest corrections for typos

--- core/src/main/kotlin/io/specmatic/core/UnexpectedKeyCheck.kt
+++ core/src/main/kotlin/io/specmatic/core/UnexpectedKeyCheck.kt
@@ -4,7 +4,7 @@ import io.specmatic.core.pattern.Pattern
 import io.specmatic.core.value.StringValue
 
 interface UnexpectedKeyCheck {
-    fun validate(pattern: Map<String, Any>, actual: Map<String, Any>): UnexpectedKeyError?
-    fun validateList(pattern: Map<String, Any>, actual: Map<String, Any>): List<UnexpectedKeyError>
-    fun validateListCaseInsensitive(pattern: Map<String, Pattern>, actual: Map<String, StringValue>): List<UnexpectedKeyError>
+    fun validate(pattern: Map<String, Any>, actual: Map<String, Any>): KeyError?
+    fun validateList(pattern: Map<String, Any>, actual: Map<String, Any>): List<KeyError>
+    fun validateListCaseInsensitive(pattern: Map<String, Pattern>, actual: Map<String, StringValue>): List<KeyError>
 }
