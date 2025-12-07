ba88b7ee100e3392f5b8872d1e1074f37d54c8bb
Sufiyan <StarKhan6368@gmail.com>
2025-12-07 10:43:53 +0530

    Implement FuzzyUnexpectedKeyCheck and example validator
    - Can validate rawJSON values against the external example schema
    - FuzzyUnexpectedKeyCheck can suggest corrections for typos

--- core/src/main/kotlin/io/specmatic/core/KeyError.kt
+++ core/src/main/kotlin/io/specmatic/core/KeyError.kt
@@ -28,3 +28,14 @@ data class UnexpectedKeyError(override val name: String) : KeyError {
     override fun missingOptionalKeyToResult(keyLabel: String, mismatchMessages: MismatchMessages): Failure =
         Failure(mismatchMessages.unexpectedKey(keyLabel, name))
 }
+
+data class FuzzyKeyError(override val name: String, val candidate: String, val isWarning: Boolean) : KeyError {
+    override fun missingKeyToResult(keyLabel: String, mismatchMessages: MismatchMessages): Failure {
+        return missingOptionalKeyToResult(keyLabel, mismatchMessages)
+    }
+
+    override fun missingOptionalKeyToResult(keyLabel: String, mismatchMessages: MismatchMessages): Failure {
+        val msg = mismatchMessages.unexpectedKey(keyLabel, name) + ", Did you mean \"$candidate\" ?"
+        return Failure(msg, isPartial = isWarning)
+    }
+}
