2decb332d06f53078c36c431aad6f9944bc68b8d
jaydeep <jaydeepk@gmail.com>
2025-12-18 12:25:51 +0530

    Reverted NotStubbed response to a 'no op'. Returned distinct list of specConfigs. Removed adding logs when creating "not covered" CtrfTestResultRecords. CtrfTestResultRecords will not be created for internal stub endpoint hits like setting expectations etc.

--- core/src/main/kotlin/io/specmatic/stub/NotStubbed.kt
+++ core/src/main/kotlin/io/specmatic/stub/NotStubbed.kt
@@ -5,18 +5,7 @@ import io.specmatic.stub.report.StubEndpoint
 
 class NotStubbed(override val response: HttpStubResponse) : StubbedResponseResult {
     override fun log(logs: MutableList<StubEndpoint>, httpRequest: HttpRequest) {
-        logs.add(
-            StubEndpoint(
-                response.scenario?.path ?: httpRequest.path,
-                httpRequest.method,
-                response.response.status,
-                response.scenario?.requestContentType ?: httpRequest.contentType(),
-                response.feature?.sourceProvider,
-                response.feature?.sourceRepository,
-                response.feature?.sourceRepositoryBranch,
-                response.feature?.specification,
-                response.feature?.serviceType ?: "HTTP",
-            )
-        )
+        // No op
+        // We do not log requests that were not stubbed
     }
 }
