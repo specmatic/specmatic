bce92adbae4d7006ee39ca50372e0a9000c878dd
Sufiyan <StarKhan6368@gmail.com>
2026-01-01 15:07:34 +0530

    Modify ProxyConfig to be simpler in SpecmaticConfig
    - Simplify ProxyOperation matching logic make it robust using patterns

--- core/src/main/kotlin/io/specmatic/core/ProxyConfig.kt
+++ core/src/main/kotlin/io/specmatic/core/ProxyConfig.kt
@@ -1,50 +1,9 @@
 package io.specmatic.core
 
 import net.minidev.json.annotate.JsonIgnore
-import java.net.URL
-import javax.activation.MimeType
+import java.io.File
 
-data class ProxyMockedOperation(val path: String, val method: String, val requestContentType: String? = null) {
+data class ProxyConfig(val port: Int, val targetUrl: String, val consumes: List<String> = emptyList()) {
     @JsonIgnore
-    fun identity() = Triple(
-        path,
-        method.lowercase(),
-        requestContentType?.let(::MimeType)
-    )
-}
-
-data class ProxyMockedSpecification(val spec: String, val operations: List<ProxyMockedOperation>) {
-    @JsonIgnore
-    fun merge(other: ProxyMockedSpecification): ProxyMockedSpecification {
-        require(spec == other.spec)
-        val mergedOperations = (operations + other.operations).distinctBy(ProxyMockedOperation::identity)
-        return copy(operations = mergedOperations)
-    }
-}
-
-data class ProxyConfig(val baseUrl: String, val targetBaseUrl: String, val mocked: List<ProxyMockedSpecification> = emptyList()) {
-    @JsonIgnore
-    fun baseUrl(): URL = URL(baseUrl)
-
-    @JsonIgnore
-    fun targetBaseUrl(): URL = URL(targetBaseUrl)
-
-    @JsonIgnore
-    fun merge(other: ProxyConfig): ProxyConfig {
-        require(baseUrl == other.baseUrl)
-        require(targetBaseUrl == other.targetBaseUrl)
-        return copy(
-            mocked = (mocked + other.mocked).groupBy(ProxyMockedSpecification::spec).map { (_, specs) ->
-                specs.reduce(ProxyMockedSpecification::merge)
-            }
-        )
-    }
-
-    companion object {
-        fun List<ProxyConfig>.merge(other: List<ProxyConfig>): List<ProxyConfig> {
-            return this.plus(other).groupBy { it.baseUrl() to it.targetBaseUrl() }.map { (_, configs) ->
-                configs.reduce(ProxyConfig::merge)
-            }
-        }
-    }
+    fun mockSpecifications(): List<File> = consumes.map(::File).map(File::getCanonicalFile)
 }
