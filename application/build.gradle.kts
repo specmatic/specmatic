a4b0c2378f4af032d16d670b2539d2d30cfcf5e6
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-01-02 19:25:47 +0530

    chore: remove unused dependencies
    This shaves off about 7MB

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -9,22 +9,16 @@ dependencies {
         exclude(group = "commons-logging", module = "commons-logging")
     }
 
-    implementation("joda-time:joda-time:2.14.0")
-    implementation("net.minidev:json-smart:2.6.0")
+    implementation("io.netty:netty-codec-http:4.2.7.Final")
 
     implementation("com.ezylang:EvalEx:3.6.0")
-    implementation("com.arakelian:java-jq:2.0.0")
-    testImplementation("com.arakelian:java-jq:2.0.0")
 
     implementation("org.assertj:assertj-core:3.27.6")
     implementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
 
-    implementation("info.picocli:picocli:4.7.7")
     implementation("io.ktor:ktor-client-core-jvm:2.3.13")
     implementation("io.ktor:ktor-network-tls:2.3.13")
     implementation("io.ktor:ktor-network-tls-certificates:2.3.13")
-
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.21.0")
 
     implementation("org.junit.platform:junit-platform-launcher:1.13.4")
     implementation("org.junit.platform:junit-platform-reporting:1.13.4")
@@ -32,17 +26,11 @@ dependencies {
     implementation("org.eclipse.jgit:org.eclipse.jgit:7.4.0.202509020913-r")
     implementation("org.eclipse.jgit:org.eclipse.jgit.ssh.apache:7.4.0.202509020913-r")
 
-    implementation("org.apache.ant:ant-junit:1.10.15")
-
     implementation(project(":specmatic-core"))
     implementation(project(":junit5-support"))
     implementation(project(":specmatic-mcp"))
 
     implementation("io.ktor:ktor-client-cio:2.3.13")
-    implementation("io.swagger.parser.v3:swagger-parser:2.1.36") {
-        exclude(group = "org.mozilla", module = "rhino")
-    }
-    implementation("org.mozilla:rhino:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.6.3")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
--- specmatic-mcp/build.gradle.kts
+++ specmatic-mcp/build.gradle.kts
@@ -12,16 +12,11 @@ dependencies {
     implementation("io.ktor:ktor-serialization-jackson:2.3.13")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.13")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.21.0")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.21.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.21.0")
-
     implementation("org.junit.platform:junit-platform-launcher:1.13.4")
     implementation("org.junit.platform:junit-platform-reporting:1.13.4")
     implementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
     implementation("org.junit.jupiter:junit-jupiter-engine:5.13.4")
     implementation("org.assertj:assertj-core:3.27.6")
 
-    implementation("info.picocli:picocli:4.7.7")
     implementation("org.fusesource.jansi:jansi:2.4.2")
 }
