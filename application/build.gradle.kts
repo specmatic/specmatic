8a49565bcdab7620e07dab21f9483dec3d2c9b8c
Naresh Jain <nashjain@gmail.com>
2026-01-31 11:04:42 +0530

    chore: upgrade kotlinx-serialization-json-jvm to version 1.10.0

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
         exclude(group = "org.mozilla", module = "rhino")
     }
     implementation("org.mozilla:rhino:1.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.10.0")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.25")
 
