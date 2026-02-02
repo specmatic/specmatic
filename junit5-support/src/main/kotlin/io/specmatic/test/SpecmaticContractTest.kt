cd33e5a795235d267c26d9f908d69a35e6fcd0be
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-02-02 15:49:39 +0530

    feat: implement an interface that provides capability to return a stream of tests and generate reports.
    This interface can be implemented by each test runner to simplify setting up test runners.

--- junit5-support/src/main/kotlin/io/specmatic/test/SpecmaticContractTest.kt
+++ junit5-support/src/main/kotlin/io/specmatic/test/SpecmaticContractTest.kt
@@ -1,39 +1,28 @@
 package io.specmatic.test
 
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
-import org.junit.jupiter.api.extension.AfterTestExecutionCallback
-import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.jupiter.api.extension.ExtensionContext
 import java.util.stream.Stream
-import kotlin.jvm.optionals.getOrNull
 
-@Volatile
-private var contractTestHarnessInstance: SpecmaticJUnitSupport? = null
-
-@ExtendWith(AfterSpecmaticContractTestExecutionCallback::class)
-interface SpecmaticContractTest {
-    @TestFactory
-    fun contractTest(): Stream<DynamicTest> {
+interface SpecmaticContractTest : SpecmaticContractTestRunner {
+    companion object {
         val instance = SpecmaticJUnitSupport()
 
-        synchronized(SpecmaticJUnitSupport::class.java) {
-            contractTestHarnessInstance = instance
+        @JvmStatic
+        @AfterAll
+        fun tearDown() {
+            instance.report()
         }
+    }
+
+    @TestFactory
+    override fun testStream(): Stream<DynamicTest> {
         return instance.contractTest()
     }
-}
 
-class AfterSpecmaticContractTestExecutionCallback : AfterTestExecutionCallback {
-    override fun afterTestExecution(context: ExtensionContext?) {
-        val testInstance = context?.testInstance?.getOrNull() as? SpecmaticJUnitSupport
-
-        if(testInstance != null) {
-            testInstance.report()
-        } else {
-            synchronized(SpecmaticJUnitSupport::class.java) {
-                contractTestHarnessInstance?.report()
-            }
-        }
+    override fun generateReports() {
+        instance.report()
     }
 }
+
