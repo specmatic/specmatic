5a5bf3c0ad7564950cb715dd60eb50694c96b58a
Sufiyan <StarKhan6368@gmail.com>
2026-02-10 19:17:25 +0530

    Add helpers to Test and Mock Config V2
    - Add helpers to update SpecExecutionConfig
    - Methods to update baseUrl and updatePort

--- core/src/main/kotlin/io/specmatic/core/config/OpenAPIMockConfig.kt
+++ core/src/main/kotlin/io/specmatic/core/config/OpenAPIMockConfig.kt
@@ -1,6 +1,7 @@
 package io.specmatic.core.config
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
@@ -10,16 +11,29 @@ data class OpenAPIMockConfig(
     @JsonProperty("examples")
     val examples: List<String>? = null
 ) {
+    private fun writeConfig(): Map<String, Any> = mapper.convertValue(this, stringMapType)
 
     companion object {
+        private val mapper = ObjectMapper().registerKotlinModule()
+        private val stringMapType = mapper.typeFactory.constructType(object : TypeReference<Map<String, Any>>() {})
+
         fun from(config: Map<String, Any>): OpenAPIMockConfig {
-            try {
-                return ObjectMapper().registerKotlinModule().convertValue(
-                    config,
-                    OpenAPIMockConfig::class.java
-                )
+            return try {
+                readConfig(config).getOrThrow()
             } catch(t: Throwable) {
                 throw IllegalArgumentException("Invalid config provided in openapi mock: ${t.message}")
+            }
+        }
+
+        fun updateWithPort(data: Map<String, Any>, port: Int): Map<String, Any> {
+            val config = readConfig(data).getOrElse { OpenAPIMockConfig(baseUrl = "") }
+            val updatedConfig = config.copy(baseUrl = "http//0.0.0.0:$port")
+            return updatedConfig.writeConfig()
+        }
+
+        private fun readConfig(data: Map<String, Any>): Result<OpenAPIMockConfig> {
+            return runCatching {
+                mapper.convertValue(data, OpenAPIMockConfig::class.java)
             }
         }
     }
--- core/src/main/kotlin/io/specmatic/core/config/OpenAPITestConfig.kt
+++ core/src/main/kotlin/io/specmatic/core/config/OpenAPITestConfig.kt
@@ -1,6 +1,7 @@
 package io.specmatic.core.config
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.specmatic.core.ResiliencyTestsConfig
@@ -13,16 +14,29 @@ data class OpenAPITestConfig(
     @JsonProperty("examples")
     val examples: List<String>? = null
 ) {
+    private fun writeConfig(): Map<String, Any> = mapper.convertValue(this, stringMapType)
 
     companion object {
+        private val mapper = ObjectMapper().registerKotlinModule()
+        private val stringMapType = mapper.typeFactory.constructType(object : TypeReference<Map<String, Any>>() {})
+
         fun from(config: Map<String, Any>): OpenAPITestConfig {
-            try {
-                return ObjectMapper().registerKotlinModule().convertValue(
-                    config,
-                    OpenAPITestConfig::class.java
-                )
+            return try {
+                readConfig(config).getOrThrow()
             } catch (t: Throwable) {
                 throw IllegalArgumentException("Invalid config provided in openapi test: ${t.message}")
+            }
+        }
+
+        fun updateWithBaseUrl(data: Map<String, Any>, baseUrl: String, resiliencyTestsConfig: ResiliencyTestsConfig): Map<String, Any> {
+            val config = readConfig(data).getOrElse { OpenAPITestConfig(baseUrl = "") }
+            val updatedConfig = config.copy(baseUrl = baseUrl, resiliencyTests = resiliencyTestsConfig)
+            return updatedConfig.writeConfig()
+        }
+
+        private fun readConfig(data: Map<String, Any>): Result<OpenAPITestConfig> {
+            return runCatching {
+                mapper.convertValue(data, OpenAPITestConfig::class.java)
             }
         }
     }
