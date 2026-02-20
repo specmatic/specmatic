53137b5ae8accc0d6680a258d7cf0d107dd1afa0
StarKhan6368 <starkhan6368@gmail.com>
2026-02-21 00:19:53 +0530

    Change SuccessCriterion propertyName to avoid conflicts
    - Add simple test for jacksom serialization for the same

--- core/src/main/kotlin/io/specmatic/core/config/v3/specmatic/Governance.kt
+++ core/src/main/kotlin/io/specmatic/core/config/v3/specmatic/Governance.kt
@@ -1,13 +1,14 @@
 package io.specmatic.core.config.v3.specmatic
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.specmatic.core.ReportConfiguration
 import io.specmatic.core.SuccessCriteria
 
-data class Governance(val report: Report? = null, val successCriteria: SuccessCriterion? = null): ReportConfiguration {
+data class Governance(val report: Report? = null, @field:JsonProperty("successCriteria") val successCriterion: SuccessCriterion? = null) : ReportConfiguration {
     @JsonIgnore
     override fun getSuccessCriteria(): SuccessCriteria {
-        return this.successCriteria?.toSuccessCriteria() ?: SuccessCriteria.default
+        return this.successCriterion?.toSuccessCriteria() ?: SuccessCriteria.default
     }
 
     override fun excludedOpenAPIEndpoints(): List<String> {
