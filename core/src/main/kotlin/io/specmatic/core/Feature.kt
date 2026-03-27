b728897352664565be5a2b6a31983616df27b4de
StarKhan6368 <starkhan6368@gmail.com>
2026-03-27 10:07:41 +0530

    Add tests for Feature testGeneration with Decisions

--- core/src/main/kotlin/io/specmatic/core/Feature.kt
+++ core/src/main/kotlin/io/specmatic/core/Feature.kt
@@ -211,6 +211,13 @@ data class Feature(
         return this.copy(scenarios = this.scenarios.map { scenario -> scenario.copy(dictionary = dictionary) })
     }
 
+    fun disableGenerativeTesting(): Feature {
+        return this.copy(
+            flagsBased = this.flagsBased.copy(generation = NonGenerativeTests, positivePrefix = "", negativePrefix = ""),
+            specmaticConfig = specmaticConfig.disableResiliencyTests()
+        )
+    }
+
     fun enableGenerativeTesting(onlyPositive: Boolean = false): Feature {
         return this.copy(
             flagsBased = this.flagsBased.copy(
@@ -834,7 +841,7 @@ data class Feature(
                 return@flatMap Decision.Execute(generatedScenarioReturnValue, originalScenario, reasoning)
             }
 
-            Decision.Skip(context = originalScenario, reasoning = Reasoning(mainReason = TestRuleViolations.ACCEPT_MISMATCH))
+            Decision.Skip(context = generatedScenarioReturnValue.value, reasoning = Reasoning(mainReason = TestRuleViolations.ACCEPT_MISMATCH))
         }
     }
 
@@ -843,7 +850,7 @@ data class Feature(
         originalScenarios: List<Scenario> = scenarios,
         fn: (Scenario, Row) -> Scenario = { s, _ -> s }
     ): Sequence<ContractTest> {
-        val scenarioDecisions = originalScenarios.asSequence().map { Decision.Execute(it, it) }
+        val scenarioDecisions = originalScenarios.asSequence().map { Decision.execute(it) }
         return generateContractTestsWithDecision(suggestions, originalScenarios, scenarioDecisions, fn).mapNotNull { decision ->
             if (decision !is Decision.Execute) return@mapNotNull null
             return@mapNotNull decision.value
@@ -851,9 +858,9 @@ data class Feature(
     }
 
     fun generateContractTestsWithDecision(
-        suggestions: List<Scenario>,
-        originalScenarios: List<Scenario>,
-        scenarios: Sequence<Decision<Scenario, Scenario>>,
+        suggestions: List<Scenario> = emptyList(),
+        originalScenarios: List<Scenario> = this.scenarios,
+        scenarios: Sequence<Decision<Scenario, Scenario>> = emptySequence(),
         fn: (Scenario, Row) -> Scenario = { s, _ -> s },
     ): Sequence<Decision<ContractTest, Scenario>> {
         val workflow = Workflow(specmaticConfig.getWorkflowDetails() ?: WorkflowDetails.default)
@@ -1003,7 +1010,7 @@ data class Feature(
         fn: (Scenario, Row) -> Scenario = { s, _ -> s },
         originalScenarios: List<Scenario> = scenarios
     ): Sequence<Pair<Scenario, ReturnValue<Scenario>>> {
-        val scenarioDecisions = originalScenarios.asSequence().map { Decision.Execute(it, it) }
+        val scenarioDecisions = originalScenarios.asSequence().map { Decision.execute(it) }
         return generateContractTestScenariosWithDecision(suggestions, originalScenarios,scenarioDecisions, fn).mapNotNull { decision ->
             if (decision !is Decision.Execute) return@mapNotNull null
             return@mapNotNull Pair(decision.context, decision.value)
@@ -1011,9 +1018,9 @@ data class Feature(
     }
 
     fun generateContractTestScenariosWithDecision(
-        suggestions: List<Scenario>,
-        originalScenarios: List<Scenario>,
-        scenarios: Sequence<Decision<Scenario, Scenario>>,
+        suggestions: List<Scenario> = emptyList(),
+        originalScenarios: List<Scenario> = emptyList(),
+        scenarios: Sequence<Decision<Scenario, Scenario>> = emptySequence(),
         fn: (Scenario, Row) -> Scenario = { s, _ -> s },
     ): Sequence<Decision<ReturnValue<Scenario>, Scenario>> {
         val positiveTestScenarios = positiveTestScenariosWithDecision(suggestions, scenarios, fn)
@@ -1048,7 +1055,7 @@ data class Feature(
                 ).map { generatedScenario ->
                     val scenarioWithPrefix = scenario.copy(generativePrefix = flagsBased.positivePrefix)
                     val returnValueWithDescription = getScenarioWithDescription(generatedScenario)
-                    val updatedReasoning = updatePositiveGenerationReasoning(resolverStrategies, generatedScenario, reasoning)
+                    val updatedReasoning = updatePositiveGenerationReasoning(generatedScenario, reasoning)
                     Decision.Execute(returnValueWithDescription, scenarioWithPrefix, updatedReasoning)
                 }
             }
@@ -1056,7 +1063,7 @@ data class Feature(
     }
 
     fun negativeTestScenarios(originalScenarios: List<Scenario> = scenarios): Sequence<Pair<Scenario, ReturnValue<Scenario>>> {
-        val scenarioDecisions = originalScenarios.asSequence().map { Decision.Execute(it, it) }
+        val scenarioDecisions = originalScenarios.asSequence().map { Decision.execute(it) }
         return negativeTestScenariosWithDecision(scenarioDecisions, originalScenarios).mapNotNull { decision ->
             if (decision !is Decision.Execute) return@mapNotNull null
             return@mapNotNull Pair(decision.context, decision.value)
@@ -1089,10 +1096,9 @@ data class Feature(
         }
     }
 
-    private fun updatePositiveGenerationReasoning(flagsBased: FlagsBased, scenario: ReturnValue<Scenario>, reasoning: Reasoning): Reasoning {
+    private fun updatePositiveGenerationReasoning(scenario: ReturnValue<Scenario>, reasoning: Reasoning): Reasoning {
         if (scenario !is HasValue || scenario.value.generatedFrom == GeneratedScenarioOrigin.EXAMPLE_ROW) return reasoning
-        if (flagsBased.generation !is GenerativeTestsEnabled) return reasoning
-        if (!specmaticConfig.isResiliencyTestingEnabled()) return reasoning
+        if (specmaticConfig.getResiliencyTestsEnabled() == ResiliencyTestSuite.none) return reasoning
         return reasoning.withMainReason(TestExecutionReason.executedPositiveGen())
     }
 
