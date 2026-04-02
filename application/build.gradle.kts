c2b43f2ea758eabc76c5e76ac473418173985eea
Specmatic Renovate <github-service-account@specmatic.io>
2026-04-02 00:35:58 +0000

    chore(deps): update jgit to v7.6.0.202603022253-r

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -29,8 +29,8 @@ dependencies {
     implementation("org.junit.platform:junit-platform-launcher:1.14.3")
     implementation("org.junit.platform:junit-platform-reporting:1.14.3")
 
-    implementation("org.eclipse.jgit:org.eclipse.jgit:7.4.0.202509020913-r")
-    implementation("org.eclipse.jgit:org.eclipse.jgit.ssh.apache:7.4.0.202509020913-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:7.6.0.202603022253-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit.ssh.apache:7.6.0.202603022253-r")
 
     implementation("org.apache.ant:ant-junit:1.10.15")
 
