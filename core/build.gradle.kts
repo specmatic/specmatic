ef8ba0a1bad4d9d5817c91b33c25f607dda42c3d
Specmatic Renovate <github-service-account@specmatic.io>
2026-04-16 00:44:19 +0000

    [SECURITY] Update dependency org.thymeleaf:thymeleaf to v3.1.4.RELEASE

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     testImplementation("io.mockk:mockk-jvm:1.14.9")
     testImplementation("org.assertj:assertj-core:3.27.7")
     testImplementation("io.ktor:ktor-client-mock-jvm:2.3.13")
-    implementation("org.thymeleaf:thymeleaf:3.1.3.RELEASE")
+    implementation("org.thymeleaf:thymeleaf:3.1.4.RELEASE")
     implementation("org.junit.platform:junit-platform-launcher:1.14.3")
 }
 
