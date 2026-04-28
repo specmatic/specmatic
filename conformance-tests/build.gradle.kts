7b6203d1a0f447720e9fce9d08f2cba70cd25ab7
specmatic-builder <github-service-account@specmatic.io>
2026-04-28 06:17:45 +0530

    chore(deps): update junit-framework monorepo (#2451)

--- conformance-tests/build.gradle.kts
+++ conformance-tests/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 dependencies {
     implementation("ch.qos.logback:logback-core:1.5.32")
     implementation("org.slf4j:slf4j-api:2.0.17")
-    implementation("org.junit.platform:junit-platform-launcher:1.14.3")
+    implementation("org.junit.platform:junit-platform-launcher:1.14.4")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.21.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.21.2")
     implementation("io.swagger.parser.v3:swagger-parser:${project.property("swaggerParserVersion")}")
@@ -12,11 +12,11 @@ dependencies {
 
     runtimeOnly("ch.qos.logback:logback-classic:1.5.32")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.4")
 
     testImplementation("org.assertj:assertj-core:3.27.7")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.14.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.14.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.14.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.14.4")
     testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
