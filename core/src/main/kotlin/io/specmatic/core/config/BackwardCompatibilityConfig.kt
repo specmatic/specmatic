1a60c71f1305494b24cd18b10d2a05fbe0572ea7
StarKhan6368 <starkhan6368@gmail.com>
2026-05-15 15:53:33 +0530

    Make BackwardCompatibilityConfig template-aware

--- core/src/main/kotlin/io/specmatic/core/config/BackwardCompatibilityConfig.kt
+++ core/src/main/kotlin/io/specmatic/core/config/BackwardCompatibilityConfig.kt
@@ -1,8 +1,43 @@
 package io.specmatic.core.config
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import io.specmatic.core.config.v3.TemplateOrValue
+import io.specmatic.core.config.v3.resolveOrNull
+import io.specmatic.core.config.v3.wrapOrNull
+
 data class BackwardCompatibilityConfig(
-    val baseBranch: String? = null,
-    val targetPath: String? = null,
-    val repoDirectory: String? = null,
-    val strictMode: Boolean? = null
-)
+    val baseBranch: TemplateOrValue<String>? = null,
+    val targetPath: TemplateOrValue<String>? = null,
+    val repoDirectory: TemplateOrValue<String>? = null,
+    val strictMode: TemplateOrValue<Boolean>? = null
+) {
+    @get:JsonIgnore
+    val resolvedBaseBranch: String?
+        get() = baseBranch.resolveOrNull()
+
+    @get:JsonIgnore
+    val resolvedTargetPath: String?
+        get() = targetPath.resolveOrNull()
+
+    @get:JsonIgnore
+    val resolvedRepoDirectory: String?
+        get() = repoDirectory.resolveOrNull()
+
+    @get:JsonIgnore
+    val resolvedStrictMode: Boolean?
+        get() = strictMode.resolveOrNull()
+
+    companion object {
+        fun from(
+            baseBranch: String? = null,
+            targetPath: String? = null,
+            repoDirectory: String? = null,
+            strictMode: Boolean? = null
+        ): BackwardCompatibilityConfig = BackwardCompatibilityConfig(
+            baseBranch = baseBranch.wrapOrNull(),
+            targetPath = targetPath.wrapOrNull(),
+            repoDirectory = repoDirectory.wrapOrNull(),
+            strictMode = strictMode.wrapOrNull()
+        )
+    }
+}
