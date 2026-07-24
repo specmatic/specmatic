ba45ce376583c292ff0e1ba8a764473fa90ea67e
Specmatic Renovate <github-service-account@specmatic.io>
2026-07-24 00:42:29 +0000

    Update logback monorepo to v1.6.0

--- conformance-tests/build.gradle.kts
+++ conformance-tests/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm")
 }
 dependencies {
-    implementation("ch.qos.logback:logback-core:1.5.37")
+    implementation("ch.qos.logback:logback-core:1.6.0")
     implementation("org.slf4j:slf4j-api:2.0.18")
     implementation("org.junit.platform:junit-platform-launcher:1.14.4")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.22.1")
@@ -11,7 +11,7 @@ dependencies {
     implementation("info.picocli:picocli:4.7.7")
     implementation("com.networknt:json-schema-validator:2.0.1")
 
-    runtimeOnly("ch.qos.logback:logback-classic:1.5.37")
+    runtimeOnly("ch.qos.logback:logback-classic:1.6.0")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.4")
 
