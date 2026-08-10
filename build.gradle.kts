229a3902324347d027f6206fcc6afe61f7d5b927
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-08-10 21:21:48 +0530

    fix(build): third time is a charm

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,6 @@ specmatic {
             "https://repo.specmatic.io/releases",
             RepoType.PUBLISH_OBFUSCATED_ONLY
         )
-        dockerImage("specmatic/specmatic-snapshots", "specmatic/specmatic:$version")
         dockerImage("specmatic/specmatic-snapshots", "specmatic/specmatic")
     }
 
