5d571c9298a7e7edf40c237cdc5a43a427a76673
StarKhan6368 <starkhan6368@gmail.com>
2026-08-14 17:56:50 +0530

    Add simple methods to check Substitution
    - Ability to check if string isOrContains capture or use expression
    - Add simple tests for the same

--- core/src/main/kotlin/io/specmatic/core/Substitution.kt
+++ core/src/main/kotlin/io/specmatic/core/Substitution.kt
@@ -2,6 +2,7 @@ package io.specmatic.core
 
 import io.specmatic.core.pattern.Pattern
 import io.specmatic.core.pattern.ReturnValue
+import io.specmatic.core.substitution.InterpolatedSubstitution
 import io.specmatic.core.value.Value
 
 interface Substitution {
@@ -9,4 +10,9 @@ interface Substitution {
     fun substitute(value: Value): ReturnValue<Value>
     fun substitute(value: Value, pattern: Pattern, resolver: Resolver = Resolver(), key: String? = null): ReturnValue<Value>
     fun upsertStoreUsing(originalValue: Value, runningValue: Value, resolver: Resolver = Resolver()): Substitution
+
+    companion object {
+        fun isOrContainsLookup(text: String): Boolean = InterpolatedSubstitution.containsLookup(text)
+        fun isOrContainsCapture(text: String): Boolean = InterpolatedSubstitution.containsCapture(text)
+    }
 }
